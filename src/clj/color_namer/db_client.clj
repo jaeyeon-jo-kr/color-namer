6a9dd3d3f27961cfc026642b579caf9efb57ddb0
jaeyeon-jo-kr <entertm77@naver.com>
2021-08-06 20:37:45 +0900

    data loading

--- src/clj/color_namer/db_client.clj
+++ src/clj/color_namer/db_client.clj
@@ -3,7 +3,10 @@
 
 (def db-uri "datomic:mem://foo")
 
-(def conn (d/connect db-uri))
+
+(def conn (atom nil))
+
+
 
 (def color-name-schema[{:db/ident :color/name
                         :db/valueType :db.type/string
@@ -20,7 +23,7 @@
 
 
 (defn insert-color-names[color-names]
-  @(d/transact conn color-names))
+  @(d/transact @conn color-names))
 
 (defn rgb->long[red green blue]
   (+ (* red 65536) (* green 256) blue))
@@ -28,26 +31,27 @@
 (defn add-sample-colors[]
   (let [colors [{:color/id 1 :color/name "red" :color/code (rgb->long 255 0 0)}
                 {:color/id 2 :color/name "blue" :color/code (rgb->long 0 0 255)}]]
-    @(d/transact conn colors)))
+    @(d/transact @conn colors)))
 
 (defn add-colors[colors]
-  @(d/transact conn colors))
+  @(d/transact @conn colors))
 
 
 (defn find-by-color-id[id]
   (let [q `[:find ~'?name, ~'?code
             :where [~'?color :color/name ~'?name] [~'?color :color/id ~id] [~'?color :color/code ~'?code]]]
     (print q)
-    (d/q q (d/db conn))))
+    (d/q q (d/db @conn))))
 
 (defn find-all[]
   (let [q '[:find ?name, ?code, ?id
             :where [?color :color/name ?name] [?color :color/id ?id] [?color :color/code ?code]]]
-    (d/q q (d/db conn))))
+    (d/q q (d/db @conn))))
   
 
 (defn init[]
   (d/create-database db-uri)
-  @(d/transact conn color-name-schema))
+  (reset! conn (d/connect db-uri))
+  @(d/transact @conn color-name-schema))
 
 
